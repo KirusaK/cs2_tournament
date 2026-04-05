6b676f7b980a2dd1fa3d4784101438be63ad14b7
KirusaK <kirusaklimenko@gmail.com>
2026-04-05 20:10:44 +0100

    feat: add Team modal and reuse styles for admin forms

--- frontend/src/features/admin-panel/AddTeamsForm/AddTeamsForm.jsx
+++ frontend/src/features/admin-panel/AddTeamsForm/AddTeamsForm.jsx
@@ -1,7 +1,6 @@
-// import sprite from "../../../../public/assets/icons/symbol-defs.svg";
 import styles from "../ModalForm.module.scss";
 
-export const AddPlayerForm = (props) => {
+export const AddTeamsForm = (props) => {
   const { onClose } = props;
 
   return (
@@ -9,7 +8,7 @@ export const AddPlayerForm = (props) => {
       <div className={styles.AddPlayerForm}>
         <div className={styles.AddPlayerForm__Wrapper}>
           <div className={styles.AddPlayerForm__Header}>
-            <h1 className={styles.AddPlayerForm__Title}>Add Player</h1>
+            <h1 className={styles.AddPlayerForm__Title}>Add Team</h1>
             <svg
               width={24}
               height={24}
@@ -22,24 +21,16 @@ export const AddPlayerForm = (props) => {
           <hr />
           <div className={styles.AddPlayerForm__Input}>
             <div className={styles.AddPlayerForm__InputWrapper}>
-              <label className={styles.AddPlayerForm__Label}>Nickname</label>
+              <label className={styles.AddPlayerForm__Label}>Team name</label>
               <input
                 type="text"
-                placeholder="Enter nickname..."
-                className={styles.AddPlayerForm__InputField}
-              />
-            </div>
-            <div className={styles.AddPlayerForm__InputWrapper}>
-              <label className={styles.AddPlayerForm__Label}>Team</label>
-              <input
-                type="text"
-                placeholder="Enter team..."
+                placeholder="Enter team name..."
                 className={styles.AddPlayerForm__InputField}
               />
             </div>
           </div>
           <div className={styles.AddPlayerForm__ButtonWrapper}>
-            <button className={styles.AddPlayerForm__Button}>ADD PLAYER</button>
+            <button className={styles.AddPlayerForm__Button}>ADD TEAM</button>
           </div>
         </div>
       </div>
--- frontend/src/pages/AdminPages/AdminPages.jsx
+++ frontend/src/pages/AdminPages/AdminPages.jsx
@@ -5,12 +5,18 @@ import { Link } from "react-router-dom";
 import { AddPlayerForm } from "../../features/admin-panel/AddPlayerForm/AddPlayerForm";
 import { useState } from "react";
 import styles from "./AdminPages.module.scss";
+import { AddTeamsForm } from "../../features/admin-panel/AddTeamsForm/AddTeamsForm";
 
 export const AdminPages = () => {
   const [isPlayerModalOpen, setIsPlayerModalOpen] = useState(false);
+  const [isTeamModalOpen, setIsTeamModalOpen] = useState(false);
 
   const togglePlayerModal = () => {
     setIsPlayerModalOpen(!isPlayerModalOpen);
+  };
+
+  const toggleTeamModal = () => {
+    setIsTeamModalOpen(!isTeamModalOpen);
   };
 
   return (
@@ -21,13 +27,18 @@ export const AdminPages = () => {
         <div className={styles.main}>
           <div className={styles.main__Button}>
             <button onClick={togglePlayerModal}>ADD PLAYER</button>
-            <button>ADD TEAM</button>
+            <button onClick={toggleTeamModal}>ADD TEAM</button>
             <button>GENERATE TOURNAMENT</button>
           </div>
 
           {isPlayerModalOpen && (
             <AddPlayerForm onClose={() => setIsPlayerModalOpen(false)} />
           )}
+
+          {isTeamModalOpen && (
+            <AddTeamsForm onClose={() => setIsTeamModalOpen(false)} />
+          )}
+
           <hr />
           <h1 className={styles.main__Title}>Players List</h1>
           <PlayersList />
